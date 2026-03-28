da2411e2f53c1c2a60d622211dcc41b9533b06d7
tushar <161031660+tushh12@users.noreply.github.com>
2026-03-28 18:04:53 +0530

    set up native postgress connection to the backend

---
+++ backend/src/libs/db.js
@@ -0,0 +1,20 @@
+
+import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config"; // Ensures we can read the .env file
+
+// 1. Create a native Postgres connection pool
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
+
+// 2. Wrap it in the Prisma Adapter
+const adapter = new PrismaPg(pool);
+
+// 3. The Singleton pattern (so Nodemon doesn't crash your DB)
+const globalForPrisma = globalThis;
+
+// Notice we pass the adapter into the PrismaClient now!
+export const db = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
