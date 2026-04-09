7f2261f2100570bcf37c6c2d3a08bfc9b53f4488
tushar <161031660+tushh12@users.noreply.github.com>
2026-04-09 19:13:37 +0530

    addedd submission model and testcases

---
+++ backend/prisma/migrations/20260409133209_added_submission_models/migration.sql
@@ -0,0 +1,66 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `referenceSolution` on the `Problem` table. All the data in the column will be lost.
+  - Added the required column `difficulty` to the `Problem` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `referenceSolutions` to the `Problem` table without a default value. This is not possible if the table is not empty.
+  - Changed the type of `description` on the `Problem` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+
+*/
+-- AlterTable
+ALTER TABLE "Problem" DROP COLUMN "referenceSolution",
+ADD COLUMN     "difficulty" "Difficulty" NOT NULL,
+ADD COLUMN     "referenceSolutions" JSONB NOT NULL,
+DROP COLUMN "description",
+ADD COLUMN     "description" TEXT NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Submission" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "problemId" TEXT NOT NULL,
+    "sourceCode" JSONB NOT NULL,
+    "language" TEXT NOT NULL,
+    "stdin" TEXT,
+    "stdout" TEXT,
+    "stderr" TEXT,
+    "compileOutput" TEXT,
+    "status" TEXT NOT NULL,
+    "memory" TEXT,
+    "time" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Submission_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TestCaseResult" (
+    "id" TEXT NOT NULL,
+    "submissionId" TEXT NOT NULL,
+    "testcase" INTEGER NOT NULL,
+    "passed" BOOLEAN NOT NULL,
+    "stdout" TEXT,
+    "expected" TEXT NOT NULL,
+    "stderr" TEXT,
+    "compileOutput" TEXT,
+    "status" TEXT NOT NULL,
+    "memory" TEXT,
+    "time" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "TestCaseResult_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "TestCaseResult_submissionId_idx" ON "TestCaseResult"("submissionId");
+
+-- AddForeignKey
+ALTER TABLE "Submission" ADD CONSTRAINT "Submission_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Submission" ADD CONSTRAINT "Submission_problemId_fkey" FOREIGN KEY ("problemId") REFERENCES "Problem"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TestCaseResult" ADD CONSTRAINT "TestCaseResult_submissionId_fkey" FOREIGN KEY ("submissionId") REFERENCES "Submission"("id") ON DELETE CASCADE ON UPDATE CASCADE;
