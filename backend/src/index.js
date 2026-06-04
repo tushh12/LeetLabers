2956c9aee8395ba02f14576057731c082c2905de
tushar <161031660+tushh12@users.noreply.github.com>
2026-06-04 21:11:48 +0530

    added vercel url in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,8 @@ const app = express();
 
 app.use(
     cors({
-      origin: "http://localhost:5173",
+      origin: ["http://localhost:5173",
+      "https://leet-labers.vercel.app"],
       credentials: true,
     })
   );
