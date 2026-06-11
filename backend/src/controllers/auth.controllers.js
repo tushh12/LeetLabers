c17bd1c63769465e3eed347d39dbf7ed78108e3b
tushar <161031660+tushh12@users.noreply.github.com>
2026-06-11 20:10:47 +0530

    hardcoded cookies production setting

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -81,7 +81,7 @@ export const login = async (req,res) => {
         })  
         res.cookie("jwt",token,{
             httpOnly: true,
-            sameSite:"strict",
+            sameSite:"none",
             secure:process.env.NODE_ENV !== "development",
             maxAge:1000*60*60*24*7 //7days
         })    
